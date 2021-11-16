f855723cce0c5f9fa9009f81b0f4ad91eeda02f3
Chithra Priya <chithrapriyaj@gmail.com>
2021-11-15 21:02:58 -0800

    update

--- level-1/schema.sql
+++ level-1/schema.sql
@@ -7,13 +7,13 @@ CREATE TABLE review_id_table (
 );
 
 -- This table will contain only unique values
-CREATE TABLE products (
+CREATE TABLE products_table (
   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
   product_title TEXT
 );
 
 -- Customer table for first data set
-CREATE TABLE customers (
+CREATE TABLE customers_table (
   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
   customer_count INT
 );
